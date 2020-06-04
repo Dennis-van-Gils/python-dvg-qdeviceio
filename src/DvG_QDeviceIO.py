f2e5f31d59ed82479d8d1e419555d217aa04decd
Dennis van Gils <37263907+Dennis-van-Gils@users.noreply.github.com>
2020-06-05 00:18:08 +0200

    stop(): Wait a tiny amount for the other thread to have entered the QWaitCondition lock, before giving a wakingAll()

--- src/DvG_QDeviceIO.py
+++ src/DvG_QDeviceIO.py
@@ -621,6 +621,11 @@ class QDeviceIO(QtCore.QObject):
                 if self.DEBUG:
                     tprint("Worker_DAQ  %s: stop confirmed" %
                            self.dev.name, self.DEBUG_color)
+                           
+                # Wait a tiny amount for the other thread to have entered the
+                # QWaitCondition lock, before giving a wakingAll().
+                # TODO: Perhaps, this can also be done by a signal (again)
+                time.sleep(.1)
                 
                 self.outer._qwc_worker_DAQ_stopped.wakeAll()
                     
@@ -644,6 +649,11 @@ class QDeviceIO(QtCore.QObject):
                 if self.DEBUG:
                     tprint("Worker_DAQ  %s: stop confirmed" %
                            self.dev.name, self.DEBUG_color)
+                
+                # Wait a tiny amount for the other thread to have entered the
+                # QWaitCondition lock, before giving a wakingAll().
+                # TODO: Perhaps, this can also be done by a signal (again)
+                time.sleep(.1)
                 
                 self.outer._qwc_worker_DAQ_stopped.wakeAll()
 
@@ -736,7 +746,7 @@ class QDeviceIO(QtCore.QObject):
                 # Wait a tiny amount for the other thread to have entered the
                 # QWaitCondition lock, before giving a wakingAll().
                 # TODO: Perhaps, this can also be done by a signal (again)
-                time.sleep(.01)
+                time.sleep(.1)
                 self.outer._qwc_worker_DAQ_stopped.wakeAll()
 
             elif self._trigger_by == DAQ_trigger.SINGLE_SHOT_WAKE_UP:
