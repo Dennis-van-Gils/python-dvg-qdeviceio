0fa773f564b98cd07b4ed1c6f8e17431367cbb14
Dennis van Gils <37263907+Dennis-van-Gils@users.noreply.github.com>
2020-06-11 22:51:47 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -56,22 +56,21 @@ master_doc = 'index'
 html_theme = 'sphinx_rtd_theme'
 
 html_theme_path = ["_themes", ]
-html_theme_options = {
-    'canonical_url': '',
-    'analytics_id': 'UA-XXXXXXX-1',  #  Provided by Google in your dashboard
-    'logo_only': False,
-    'display_version': True,
-    'prev_next_buttons_location': 'bottom',
-    'style_external_links': False,
-    #'vcs_pageview_mode': '',
-    'style_nav_header_background': '#2980B9',
+#html_theme_options = {
+#    'canonical_url': '',
+#    'analytics_id': 'UA-XXXXXXX-1',  #  Provided by Google in your dashboard
+#    'logo_only': False,
+#    'display_version': True,
+#    'prev_next_buttons_location': 'bottom',
+#    'style_external_links': False,
+#    'style_nav_header_background': '#2980B9',
     # Toc options
-    'collapse_navigation': True,
-    'sticky_navigation': True,
-    'navigation_depth': 4,
-    'includehidden': True,
-    'titles_only': False
-}
+#    'collapse_navigation': True,
+#    'sticky_navigation': True,
+#    'navigation_depth': 4,
+#    'includehidden': True,
+#    'titles_only': False
+#}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
