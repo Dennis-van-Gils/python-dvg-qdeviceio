7017826ee880fa199877dcc4c0f7af20b3893155
Dennis-van-Gils <vangils.dennis@gmail.com>
2020-06-11 10:37:31 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -44,7 +44,10 @@ templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 
+master_doc = 'index'
+
 # -- Options for HTML output -------------------------------------------------
+
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
